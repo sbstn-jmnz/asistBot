be66c57777df0bbda96625f73f148ddba6f47e24
Sebastián Jiménez <sjimenez@plastikilo.cl>
2017-08-02 12:55:20 -0400

    basic config and scrpit extraction

---
+++ chatScript.js
@@ -0,0 +1,129 @@
+var accessToken = "95824268112b491fa410233eac441b3a",
+  baseUrl = "https://api.api.ai/v1/",
+  $speechInput,
+  $recBtn,
+  recognition,
+  messageRecording = "Escuchando...",
+  messageCouldntHear = " No logré escucharte.¿Puedes repetirlo?",
+  messageInternalError = "Oh no, ha habido un problema con la conexión",
+  messageSorry = "Ohh lo siento, aún no tengo la espuesta para eso";
+
+$(document).ready(function() {
+  $speechInput = $("#speech");
+  $recBtn = $("#rec");
+
+  $speechInput.keypress(function(event) {
+    if (event.which == 13) {
+      event.preventDefault();
+      send();
+    }
+  });
+  $recBtn.on("click", function(event) {
+    switchRecognition();
+  });
+  $(".debug__btn").on("click", function() {
+    $(this).next().toggleClass("is-active");
+    return false;
+  });
+});
+
+function startRecognition() {
+  recognition = new webkitSpeechRecognition();
+  recognition.continuous = false;
+      recognition.interimResults = false;
+
+  recognition.onstart = function(event) {
+    respond(messageRecording);
+    updateRec();
+  };
+  recognition.onresult = function(event) {
+    recognition.onend = null;
+
+    var text = "";
+      for (var i = event.resultIndex; i < event.results.length; ++i) {
+        text += event.results[i][0].transcript;
+      }
+      setInput(text);
+    stopRecognition();
+  };
+  recognition.onend = function() {
+    respond(messageCouldntHear);
+    stopRecognition();
+  };
+  recognition.lang = "es-CL";
+  recognition.start();
+}
+
+function stopRecognition() {
+  if (recognition) {
+    recognition.stop();
+    recognition = null;
+  }
+  updateRec();
+}
+
+function switchRecognition() {
+  if (recognition) {
+    stopRecognition();
+  } else {
+    startRecognition();
+  }
+}
+
+function setInput(text) {
+  $speechInput.val(text);
+  send();
+}
+
+function updateRec() {
+  $recBtn.text(recognition ? "Stop" : "Speak");
+}
+
+function send() {
+  var text = $speechInput.val();
+  $.ajax({
+    type: "POST",
+    url: baseUrl + "query",
+    contentType: "application/json; charset=utf-8",
+    dataType: "json",
+    headers: {
+      "Authorization": "Bearer " + accessToken
+    },
+    data: JSON.stringify({query: text, lang: "es", sessionId: "yaydevdiner"}),
+
+    success: function(data) {
+      prepareResponse(data);
+    },
+    error: function() {
+      respond(messageInternalError);
+    }
+  });
+}
+
+function prepareResponse(val) {
+  var debugJSON = JSON.stringify(val, undefined, 2),
+    spokenResponse = val.result.speech;
+
+  respond(spokenResponse);
+  debugRespond(debugJSON);
+}
+
+function debugRespond(val) {
+  $("#response").text(val);
+}
+
+function respond(val) {
+  if (val == "") {
+    val = messageSorry;
+  }
+
+  if (val !== messageRecording) {
+    var msg = new SpeechSynthesisUtterance();
+    msg.voiceURI = "native";
+    msg.text = val;
+    msg.lang = "es-CL";
+    window.speechSynthesis.speak(msg);
+  }
+
+  $("#spokenResponse").addClass("is-active").find(".spoken-response__text").html(val);
+}
